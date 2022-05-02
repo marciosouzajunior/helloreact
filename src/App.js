60644607d577b81f9481f44529863d957aa178c7
Márcio de Souza Júnior <marciosouzajunior@gmail.com>
2022-05-02 14:23:23 -0300

    Implementing example of Road to React book

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+// executed only once
+var aux = 'test';
+
+function getGreetingMessage() {
+
+  const d = new Date();
+  let hour = d.getHours();
+
+  if (hour >= 0 && hour < 12) {
+    return 'Good morning';
+  } else if (hour >= 12 && hour <= 18) {
+    return 'Good afternoon';
+  } else {
+    return 'Good night';
+  }
+
+}
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
+
+  // executed everytime function executes
+  var message = {
+    first: 'Hello',
+    second: 'World!'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h1>{message.first} {message.second}</h1>
+      <h3>{getGreetingMessage()}</h3>
+
+      <hr />
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
+      <hr />
+
+      {/* lets render a list*/}
+      <ul>
+        {
+          list.map(function (item) {
+            return (
+              <li key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}, </a>
+                </span>
+                <span>{item.author}</span>
+              </li>
+            );
+          })
+        }
+      </ul>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
-ReactDOM.render(element, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
