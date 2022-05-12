b6fc9ed8f3f257362f3afbcf07f760e1bf647df4
Márcio de Souza Júnior <marciosouzajunior@gmail.com>
2022-05-12 15:49:12 -0300

    Fibonacci feature

--- src/App.js
+++ src/App.js
@@ -1,72 +1,17 @@
 import * as React from 'react';
 
-function getGreetingMessage() {
-
-  const d = new Date();
-  let hour = d.getHours();
-
-  if (hour >= 0 && hour < 12) {
-    return 'Good morning';
-  } else if (hour >= 12 && hour <= 18) {
-    return 'Good afternoon';
-  } else {
-    return 'Good night';
-  }
-
-}
-
 const App = () => {
-
-  var message = {
-    first: 'Hello',
-    second: 'World!'
-  };
-
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      objectID: 1,
-    },
-  ];
-
-  const [searchTerm, setSearchTerm] = React.useState('');
-
-  function handleChange(event) {
-    setSearchTerm(event.target.value);
-  }
-
-  const filteredStories = stories.filter(function (item) {
-    return item.title.includes(searchTerm);
-  });
 
   const [fibTerm, setFibTerm] = React.useState(0);
 
   function fibInputChange(event) {
-    //console.log(event.target.value);
     setFibTerm(event.target.value);
   }
 
   return (
     <div>
 
-      <h1>{message.first} {message.second}</h1>
-      <h3>{getGreetingMessage()}</h3>
-
-      <hr />
-      <Search onSearchChange={handleChange} />
-
-      <hr />
-      <List list={filteredStories} />
-
-      <h3>Fibonnaci</h3>
+      <h3>Fibonacci</h3>
 
       <hr />
       <label htmlFor="fibInput">Number of terms: </label>
@@ -79,59 +24,26 @@ const App = () => {
   );
 }
 
-const List = (props) =>
-(
-  <ul>
-    {
-      props.list.map((item) => {
-        return (
-          <Item key={item.objectID} item={item} />
-        );
-      })
-    }
-  </ul>
-);
-
-function Item(props) {
-  return (
-    <li>
-      <span>
-        <a href={props.item.url}>{props.item.title}, </a>
-      </span>
-      <span>{props.item.author}</span>
-    </li>
-  );
-}
-
-function Search(props) {
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearchChange} />
-    </div>
-  );
-}
-
 function Fib(props) {
 
-  function calcFib(term){
-    
-    if (term <= 1){
+  function calcFib(term) {
+
+    if (term <= 1) {
       return term;
     }
-    return calcFib(term-1) + calcFib(term-2);
+    return calcFib(term - 1) + calcFib(term - 2);
   }
 
   let arr = [];
-  for(let i = 0; i < props.term; i++){
+  for (let i = 0; i < props.term; i++) {
     arr.push(calcFib(i));
   }
 
   return (
     <p>
-        {arr.map(function(item){
-          return item + ", ";
-        })}
+      {arr.map(function (item) {
+        return item + ", ";
+      })}
     </p>
   );
 
